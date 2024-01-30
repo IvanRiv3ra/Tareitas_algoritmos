4fb7dd170f5278c618b6cadc5818c3a9daa6aff3
IvanRiv3ra <143676353+IvanRiv3ra@users.noreply.github.com>
2024-01-30 17:09:36 -0600

    Add files via upload

---
+++ programa_datos_personales.py
@@ -0,0 +1,9 @@
+#nombre = input("Ingresa tu nombre: ")
+#control = input("Ingresa tu número de control: ")
+
+nombre = "García Rivera Ángel Iván"
+control = 210120106
+
+print("\nNombre:" + nombre)
+print("Número de control:", control)
+
